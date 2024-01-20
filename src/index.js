3f0d07238750b70cf60aca716f1002d6f20c09ec
Arnab Golder <arnabgolder7@gmail.com>
2024-01-21 00:16:50 +0530

    added database config..

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
 const app = express();
+const { PORT } = require("./config/serverConfig");
 
-const prepareAndStartServer = () => {
-  app.listen(3002, () => {
-    console.log("server running on port 3002");
+const prepareAndStartServer = async () => {
+  app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
   });
 };
 
